eb7033aedff3f532af3355d6850534000e19843b
takahiro <takapi854@gmail.com>
2023-01-04 23:15:22 +0900

    push

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -14,7 +14,6 @@ func CORSHeaderMiddleware(cfg *config.Config) mux.MiddlewareFunc {
 
 			w.Header().Set("Access-Control-Allow-Origin", allowOrigin)
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			//w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")  // <- CORSMethodMiddleware でつけてる。
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			next.ServeHTTP(w, req)
 		})
