5974f9f197cd25cb8673c3d8c99737509a7d306f
takahiro <takapi854@gmail.com>
2023-01-10 22:50:58 +0900

    fix server test

--- pkg/server/user_test.go
+++ pkg/server/user_test.go
@@ -45,34 +45,37 @@ func TestServer_GetUser(t *testing.T) {
 		}
 	}
 
-	type inputUser struct {
-		id string
-	}
+	t.Run("GetUser", func(t *testing.T) {
+		type inputUser struct {
+			id string
+		}
 
-	r := inputUser{
-		id: "1",
-	}
-	b, _ := json.Marshal(r)
+		t.Run("ok", func(t *testing.T) {
+			r := inputUser{
+				id: "1",
+			}
+			b, _ := json.Marshal(r)
 
-	req, err := http.NewRequest("GET", testServer.URL+`/v1/user/1`, bytes.NewReader(b))
-	assert.NoError(t, err)
-	req.Header.Set("Content-Type", "application/json")
+			req, err := http.NewRequest("GET", testServer.URL+`/v1/user/1`, bytes.NewReader(b))
+			assert.NoError(t, err)
+			req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
-	res, err := client.Do(req)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, res)
+			client := &http.Client{}
+			res, err := client.Do(req)
+			assert.NoError(t, err)
+			assert.NotEmpty(t, res)
 
-	body, err := io.ReadAll(res.Body)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, body)
-	defer res.Body.Close()
+			body, err := io.ReadAll(res.Body)
+			assert.NoError(t, err)
+			assert.NotEmpty(t, body)
+			defer res.Body.Close()
 
-	var data entity.User
-	_ = json.Unmarshal(body, &data)
-	assert.Equal(t, "1", data.ID)
-	assert.Equal(t, "user1", data.Name)
-
+			var data entity.User
+			_ = json.Unmarshal(body, &data)
+			assert.Equal(t, "1", data.ID)
+			assert.Equal(t, "user1", data.Name)
+		})
+	})
 }
 
 func TestServer_CreateUser(t *testing.T) {
@@ -88,28 +91,30 @@ func TestServer_CreateUser(t *testing.T) {
 		}
 	})
 
-	r := input.User{
-		Name: "user",
-	}
-	b, _ := json.Marshal(r)
+	t.Run("CreateUser", func(t *testing.T) {
+		r := input.User{
+			Name: "user",
+		}
+		b, _ := json.Marshal(r)
 
-	req, err := http.NewRequest("POST", testServer.URL+`/v1/user`, bytes.NewReader(b))
-	assert.NoError(t, err)
-	req.Header.Set("Content-Type", "application/json")
+		req, err := http.NewRequest("POST", testServer.URL+`/v1/user`, bytes.NewReader(b))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
-	res, err := client.Do(req)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, res)
-	assert.Equal(t, http.StatusOK, res.StatusCode)
+		client := &http.Client{}
+		res, err := client.Do(req)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, res)
+		assert.Equal(t, http.StatusOK, res.StatusCode)
 
-	body, err := io.ReadAll(res.Body)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, body)
-	defer res.Body.Close()
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, body)
+		defer res.Body.Close()
 
-	var data input.User
-	_ = json.Unmarshal(body, &data)
-	assert.Equal(t, "user", data.Name)
+		var data input.User
+		_ = json.Unmarshal(body, &data)
+		assert.Equal(t, "user", data.Name)
+	})
 
 }
